f3d8cdaba7306d5e169264c8b019c0ac8b90fc85
anonymous <anonymous>
2016-11-11 16:19:49 -0500

    REFACTOR reorganize test cases

--- convert-rpn_test.c
+++ convert-rpn_test.c
@@ -2,19 +2,14 @@
 #include <check.h>
 #include "convert-rpn.h"
 
-START_TEST(isValidOperator_withPlus_returnsTrue)
+START_TEST(isValidOperator_withValidOperators_returnsTrue)
 {
     ck_assert_int_eq(1, isValidOperator('+'));
-}
-END_TEST
-
-START_TEST(isValidOperator_withMinus_returnsTrue)
-{
     ck_assert_int_eq(1, isValidOperator('-'));
 }
 END_TEST
 
-START_TEST(isValidOperator_withModulo_returnsFalse)
+START_TEST(isValidOperator_withInvalidOperators_returnsFalse)
 {
     ck_assert_int_eq(0, isValidOperator('%'));
 }
@@ -28,9 +23,8 @@ Suite * suite_convert_rpn_create(void)
     suite = suite_create("Convert Infix to Reverse Polish Notation");
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, isValidOperator_withPlus_returnsTrue);
-    tcase_add_test(tc_core, isValidOperator_withMinus_returnsTrue);
-    tcase_add_test(tc_core, isValidOperator_withModulo_returnsFalse);
+    tcase_add_test(tc_core, isValidOperator_withValidOperators_returnsTrue);
+    tcase_add_test(tc_core, isValidOperator_withInvalidOperators_returnsFalse);
 
     suite_add_tcase(suite, tc_core);
 
