48e0305bc9e55e27c8aee54432cddf6f9e2ac86f
anonymous <anonymous>
2016-11-15 12:58:53 -0500

    CODE: calculates operator precedence of operators correctly

--- convert-rpn.c
+++ convert-rpn.c
@@ -22,7 +22,14 @@ int isValidOperand(char subject)
 
 int precedenceOf(char subject)
 {
-    return 0;
+    switch (subject) {
+    case '^': return 5;
+    case '/': return 4;
+    case '*': return 3;
+    case '-': return 2;
+    case '+': return 1;
+    default: return 0;
+    }
 }
 
 typedef enum { OPERAND, OPERATOR } SymbolType;
