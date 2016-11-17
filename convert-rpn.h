5eda1fec5e52e9ddaf972eb569d6666c52a3869d
anonymous <anonymous>
2016-11-17 16:59:17 -0500

    REFACTOR remove header references to internal methods

--- convert-rpn.h
+++ convert-rpn.h
@@ -3,9 +3,6 @@
 
 typedef enum { RPN_SUCCESS, RPN_INVALID_ARGS, RPN_PARSE_ERROR_INVALID_OPERAND, RPN_PARSE_ERROR_INVALID_OPERATOR } RpnErrorType;
 
-int isValidOperator(char subject);
-int isValidOperand(char subject);
-int precedenceOf(char subject);
 RpnErrorType infixToReversePolish(char *in, char *out, int length);
 
 #endif
