2bf7d13ca5d0ba0a61ed4b9f44ee08f3266cbccd
anonymous <anonymous>
2016-11-18 12:34:36 -0500

    REFACTOR: extract paren, operand, and operator processing procedures

--- convert-rpn.c
+++ convert-rpn.c
@@ -66,12 +66,60 @@ char pop(struct Stack *stack)
     return value;
 }
 
+int processOpenParen(struct Stack *operators, char current)
+{
+    if ('(' == current) {
+        push(operators, '(');
+        return 1;
+    }
+    return 0;
+}
+
+int processCloseParen(struct Stack *operators, char current, char *out, int *outIndex)
+{
+    char operator;
+    if (')' == current) {
+        while (!isEmpty(operators) && (operator = pop(operators)) != '(') {
+            out[*outIndex] = operator;
+            *outIndex += 1;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+RpnErrorType processOperand(char current, char *out, int *outIndex)
+{
+    if (!isValidOperand(current))
+        return RPN_PARSE_ERROR_INVALID_OPERAND;
+
+    out[*outIndex] = current;
+    *outIndex += 1;
+    return RPN_SUCCESS;
+}
+
+RpnErrorType processOperator(struct Stack *operators, char current, char *out, int *outIndex)
+{
+    if (!isValidOperator(current))
+        return RPN_PARSE_ERROR_INVALID_OPERATOR;
+
+    while (!isEmpty(operators) && precedenceOf(current) < precedenceOf(head(operators))) {
+        out[*outIndex] = pop(operators);
+        *outIndex += 1;
+    }
+
+    push(operators, current);
+
+    return RPN_SUCCESS;
+}
+
 RpnErrorType infixToReversePolish(char *in, char *out, int length)
 {
     int inIndex = 0, outIndex = 0;
-    char current, operator;
+    char current;
     char stackArray[length / 2];
     struct Stack operators = { stackArray, 0 };
+    RpnErrorType result;
     SymbolType expecting = OPERAND;
 
     if (in == NULL || out == NULL || length < 1)
@@ -86,32 +134,21 @@ RpnErrorType infixToReversePolish(char *in, char *out, int length)
 
         if (expecting == OPERAND)
         {
-            if ('(' == current) {
-                push(&operators, '(');
+            if (processOpenParen(&operators, current))
                 continue;
-            }
 
-            if (!isValidOperand(current))
-                return RPN_PARSE_ERROR_INVALID_OPERAND;
+            if ((result = processOperand(current, out, &outIndex)) != RPN_SUCCESS)
+                return result;
 
-            out[outIndex++] = current;
             expecting = OPERATOR;
         }
         else if (expecting == OPERATOR)
         {
-            if (')' == current) {
-                while (!isEmpty(&operators) && (operator = pop(&operators)) != '(')
-                    out[outIndex++] = operator;
+            if (processCloseParen(&operators, current, out, &outIndex))
                 continue;
-            }
 
-            if (!isValidOperator(current))
-                return RPN_PARSE_ERROR_INVALID_OPERATOR;
-
-            while (!isEmpty(&operators) && precedenceOf(current) < precedenceOf(head(&operators)))
-                out[outIndex++] = pop(&operators);
-
-            push(&operators, current);
+            if ((result = processOperator(&operators, current, out, &outIndex)) != RPN_SUCCESS)
+                return result;
 
             expecting = OPERAND;
         }
